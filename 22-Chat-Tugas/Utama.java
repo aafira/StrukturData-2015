76b17dc38938b4512d3e21e45fdd8900d62702d4
Aida Afira <aiplay39@gmail.com>
2015-12-07 17:45:45 +0700

    Tugas Programming 3 - Chat UPPER

---
+++ 22-Chat-Tugas/Utama.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Utama here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Utama
+{
+    public static void main(String[] args) {
+        try {
+            Chat kirim = new Chat();
+            Scanner input = new Scanner(System.in);
+            
+            System.out.print("Client: ");
+            String kalimat = new String();
+            kalimat = input.nextLine();
+            
+            kirim.chat(kalimat);
+        }
+        catch (UnknownHostException ex) {
+            System.err.println(ex);
+        }
+        catch (IOException ex) {
+            System.err.println(ex);
+        }
+    }
+
+
+}
