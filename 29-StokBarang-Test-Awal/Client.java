98e15c6082ed54ae02c43ada2a745ac7d072f3d8
Aida Afira <aiplay39@gmail.com>
2015-12-18 17:03:10 +0700

    tes awal + praktikum

---
+++ 29-StokBarang-Test-Awal/Client.java
@@ -0,0 +1,57 @@
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.BufferedReader;
+
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.io.BufferedWriter;
+
+import java.util.Scanner;
+
+public class Client {
+    public void chat() 
+                throws UnknownHostException, IOException {
+        Socket socket = new Socket("192.168.43.13", 33333);
+        try {
+          String ketikanSatuBaris;
+ 
+          do{  
+              // Ketik            
+            Scanner keyboard = new Scanner(System.in);
+            System.out.print("Perintah: ");
+            ketikanSatuBaris = keyboard.nextLine();
+            ketikanSatuBaris.toUpperCase();
+            // Tulis ke socket
+            Writer keluaranWriter = new OutputStreamWriter(socket.getOutputStream()); 
+            BufferedWriter keluaranBuff = new BufferedWriter(keluaranWriter);
+            keluaranBuff.write(ketikanSatuBaris);
+            keluaranBuff.write("\n");
+            keluaranBuff.flush();
+            
+            if((ketikanSatuBaris.charAt(0))=='j'){
+                System.out.print("Server: ");
+                Reader masukan = new InputStreamReader(socket.getInputStream()); 
+                BufferedReader masukanBuff = new BufferedReader(masukan);
+                String baris = masukanBuff.readLine();
+                System.out.println(baris);
+            }
+            
+          }while((ketikanSatuBaris.charAt(0))!='S');
+          
+          if((ketikanSatuBaris.charAt(0))=='S')
+             socket.close();
+          
+        }
+        catch(IOException salah) {
+            System.out.println(salah);
+        }
+        finally {
+            if (socket != null)
+            socket.close();
+        }
+    }
+}
---
+++ 29-StokBarang-Test-Awal/ProcessClientThread.java
@@ -0,0 +1,80 @@
+import java.net.Socket;
+import java.net.InetAddress;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.util.Scanner;
+
+public class ProcessClientThread implements Runnable {
+    int jumlah =0;
+    public ProcessClientThread(Socket koneksiKiriman) {
+        koneksi = koneksiKiriman;
+    }
+
+    public void run() {
+        if (koneksi != null)
+            prosesPermintaanClient();
+    }
+
+    private void prosesPermintaanClient() {
+      try {
+        String ip = koneksi.getInetAddress().getHostAddress();
+        System.out.println("Dari: " + ip);
+        String baris ;
+        String angkaPeriksaStr = Integer.toString(jumlah);
+
+             
+                    
+        do{
+            // Ambil dan tampilkan masukan
+            InputStream masukan = koneksi.getInputStream();
+            BufferedReader masukanReader = new BufferedReader(new InputStreamReader(masukan)); 
+            baris = masukanReader.readLine();
+            //baris.toUpperCase();
+            System.out.println(baris);   
+                        
+            if((baris.charAt(0))=='1'){
+                jumlah++;
+                System.out.println("jumlah "+jumlah); 
+            }
+            else if((baris.charAt(0))=='2'){
+                jumlah--;
+                System.out.println("jumlah "+jumlah); 
+            }
+            else if((baris.charAt(0))=='3'){
+                // Kirim ke client
+                OutputStream keluaran = koneksi.getOutputStream();
+                BufferedWriter keluaranBuf = new BufferedWriter(new OutputStreamWriter(keluaran)); 
+                angkaPeriksaStr = Integer.toString(jumlah);
+                //synchronized(angkaPeriksaStr) {
+                keluaranBuf.write(angkaPeriksaStr);
+                keluaranBuf.newLine();
+                keluaranBuf.flush();                
+                //}
+            }
+        }while((baris.charAt(0)!='4'));
+       
+
+        // Tunggu 2 detik
+        System.out.println("Tunggu...");
+        Thread.sleep(2000);
+        System.out.println("Selesai tunggu...");
+
+        koneksi.close();
+        
+        }
+        catch(IOException err) {
+            System.out.println(err);
+        }
+        catch(InterruptedException err) {
+            System.out.println(err);
+        }
+        
+    }
+
+    private Socket koneksi;
+}
